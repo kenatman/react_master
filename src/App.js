dd1cd3cd62cf3e345c73184b53dbbb29429ec87d
Yunsu <kenatman0723@gmail.com>
2021-12-19 17:36:30 +0900

    ThemeProvider in SC

--- src/App.js
+++ src/App.js
@@ -7,41 +7,17 @@ const Wrapper = styled.div`
   height: 100vh;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const Anim = keyframes`
-0%{transform:rotate(0deg);
-border-radius: 0%;
-}
-50%{
-border-radius: 50%;}
-100%{transform:rotate(360deg);
-border-radius: 0%;}}
-`;
-
-const Emoji = styled.span`
-  font-size: 50px;
-`;
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: tomato;
-  animation: ${Anim} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  ${Emoji}:hover {
-    font-size: 80px;
-  }
+const Text = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 const App = () => {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😍</Emoji>
-      </Box>
-      <Emoji>❓</Emoji>
+      <Text>Hello</Text>
     </Wrapper>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const darkMode = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const whiteMode = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
+
+ReactDOM.render(
+  <ThemeProvider theme={darkMode}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
