a7a1228dec2cf9bb21818f398ddf3ab94576c0ff
Yunsu <kenatman0723@gmail.com>
2021-12-25 20:49:22 +0900

    typescript 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+
 import { ThemeProvider } from "styled-components";
+import App from "./App";
 
 const darkMode = {
   textColor: "whitesmoke",
