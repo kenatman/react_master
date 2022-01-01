cef557278552dd1283871534eacd545b4694cb97
Yunsu <kenatman0723@gmail.com>
2022-01-01 16:48:01 +0900

    e : React.FormEvent

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
 
 const App = () => {
+  const [value, setValue] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setValue(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`hello! ${value}`);
+  };
   return (
-    <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" />
-    </div>
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="username"
+        value={value}
+        onChange={onChange}
+      />
+      <button>LOGIN</button>
+    </form>
   );
 };
 
