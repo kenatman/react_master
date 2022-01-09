efecf8ec1d45f228f3a34971975123a6f0f20cd4
Yunsu <kenatman0723@gmail.com>
2022-01-09 19:43:12 +0900

    chart pt2 : how to use Apex lib

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,16 +1,57 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
   coinId: string;
 }
 
+interface IData {
+  close: number;
+  high: number;
+  low: number;
+  market_cap: number;
+  open: number;
+  time_close: string;
+  time_open: string;
+  volume: number;
+}
+
 const Chart = ({ coinId }: ChartProps) => {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IData[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <div>CHART</div>;
+
+  return (
+    <div>
+      {isLoading ? (
+        "LOADING CHART..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[{ name: "price", data: data?.map((price) => price.close) }]}
+          options={{
+            theme: { mode: "dark" },
+            chart: {
+              width: 500,
+              height: 300,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: { curve: "smooth", width: 4 },
+            yaxis: { show: false },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Chart;
